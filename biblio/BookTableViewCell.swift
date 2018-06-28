2893c5409f7e27e049e7cc75e4008946a026ce7b
Giovanni Lodi <giovanni.lodi42@gmail.com>
2018-06-28 20:01:25 +1000

    Implement dedicated cell for books
    I wasen't able to make `.imageView` play nicely with the dynamic size.

---
+++ biblio/BookTableViewCell.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+class BookTableViewCell: UITableViewCell {
+
+  @IBOutlet var titleLabel: UILabel!
+  @IBOutlet var descriptionLabel: UILabel!
+  @IBOutlet var thumbnailImageView: UIImageView!
+
+  override func prepareForReuse() {
+    super.prepareForReuse()
+
+    thumbnailImageView.image = UIImage(
+      color: .gray,
+      size: CGSize(width: 1, height: 1)
+    )
+  }
+}
+
+public extension UIImage {
+
+  public convenience init?(color: UIColor, size: CGSize) {
+    let rect = CGRect(origin: .zero, size: size)
+    UIGraphicsBeginImageContextWithOptions(rect.size, false, 0.0)
+    color.setFill()
+    UIRectFill(rect)
+    let image = UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+
+    guard let cgImage = image?.cgImage else { return nil }
+    self.init(cgImage: cgImage)
+  }
+}
